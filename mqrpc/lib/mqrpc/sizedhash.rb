419e841926ddff34bc198dc59ca3074716c62faa
jls@semicomplete.com <jls@semicomplete.com@c74e39cc-815a-11de-a6cd-7b297ead3933>
2009-11-06 06:03:45 +0000

    - Add SizedThreadSafeHash for use with MQRPC so we can block if too many   messages are waiting on responses from any given queue.

---
+++ mqrpc/lib/mqrpc/sizedhash.rb
@@ -0,0 +1,79 @@
+require 'thread'
+
+# Thread-safe sized hash similar to SizedQueue.
+# The only time we will block execution is in setting new items.
+# That is, SizedThreadSafeHash#[]=
+class SizedThreadSafeHash
+  def initialize(size)
+    @lock = Mutex.new
+    @size = size
+    @condvar = ConditionVariable.new
+    @data = Hash.new
+  end # def initialize
+
+  # set a key and value
+  def []=(key, value)
+    @lock.synchronize do
+      # If adding a new item, wait if the hash is full
+      if !@data.has_key?(key) and _withlock_full?
+        @condvar.wait(@lock)
+      end
+      @data[key] = value
+    end
+  end # def []=
+
+  # get an value by key
+  def [](key)
+    @lock.synchronize do
+      return @data[key]
+    end
+  end # def []
+
+  # delete a key
+  def delete(key)
+    @lock.synchronize do
+      @data.delete(key)
+      @condvar.signal
+    end
+  end # def delete
+
+  # boolean, indicates true when the hash is full (has size == initialized size)
+  def full?
+    @lock.synchronize do
+      return _withlock_full?
+    end
+  end # def full?
+
+  # return the size (total number of entries) in this hash.
+  def size
+    @lock.synchronize do
+      return @data.size
+    end
+  end
+
+  # Return an array of keys for this hash.
+  def keys
+    @lock.synchronize do
+      return @data.keys
+    end
+  end
+  alias :length :size
+
+  private
+  def _withlock_full?
+    return @data.size == @size
+  end
+end # class SizedThreadSafeHash
+
+#sh = SizedThreadSafeHash.new(10)
+#Thread.new do
+  #0.upto(100) do |i|
+    #sh[i] = true
+  #end
+#end
+
+#0.upto(100) do |i|
+  #puts sh[i]
+  #sh.delete(i)
+  #sleep 1
+#end
