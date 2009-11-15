cbec8de2656cfd3fb862ca21f8e97c30ceb1cf2d
jls@semicomplete.com <jls@semicomplete.com@c74e39cc-815a-11de-a6cd-7b297ead3933>
2009-11-15 16:39:58 +0000

    - Allow 'finished' and 'finished?' to be called from outside the object

--- mqrpc/lib/mqrpc/operation.rb
+++ mqrpc/lib/mqrpc/operation.rb
@@ -6,9 +6,14 @@ module MQRPC
   # A single message operation
   # * Takes a callback to call when a message is received
   # * Allows you to wait for the operation to complete.
-  # * An operation is 'complete' when the callback returns :finished
+  # * An operation is 'complete' when the callback returns.
+  #
+  # If your callback returns :continue, then we will not call finished.
+  # This allows you to have an operation that is invoked multiple times,
+  # such as for streaming blocks of data, and only finish when you know
+  # you are done.
   class Operation
-    def initialize(callback)
+    def initialize(&callback)
       @mutex = Mutex.new
       @callback = callback
       @cv = ConditionVariable.new
@@ -21,9 +26,7 @@ module MQRPC
       @mutex.synchronize do
         ret = @callback.call(*args)
         if ret != :continue
-          #MQRPC::logger.debug "operation #{self} finished"
-          @finished = true
-          @cv.signal
+          _withlock_finished
         end
         return ret
       end
@@ -34,15 +37,35 @@ module MQRPC
     # immediately.
     def wait_until_finished
       @mutex.synchronize do
-        if !finished?
+        if !_withlock_finished?
           @cv.wait(@mutex)
         end
       end
     end # def wait_until_finished
 
-    protected
+    # Is the operation finished yet?
     def finished?
+      @mutex.synchronize do
+        return _withlock_finished?
+      end
+    end # def finished?
+
+    # Declare that the operation is finished.
+    def finished
+      @mutex.synchronize do
+        return _withlock_finished
+      end
+    end # def finished
+
+    protected
+    def _withlock_finished?
       return @finished
     end # def finished?
+
+    def _withlock_finished
+      @finished = true
+      @cv.signal
+    end
+
   end # class Operation
 end # module MQRPC
