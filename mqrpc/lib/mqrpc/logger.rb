952470c2ccf0deb1c953f3b965df6af5684675fd
jls@semicomplete.com <jls@semicomplete.com@c74e39cc-815a-11de-a6cd-7b297ead3933>
2009-11-15 07:31:08 +0000

    - Make Logger output include thread id.

--- mqrpc/lib/mqrpc/logger.rb
+++ mqrpc/lib/mqrpc/logger.rb
@@ -13,3 +13,15 @@ module MQRPC
     @logger = logger
   end
 end
+
+# Make logger include the thread id.
+class FormatterWithThread < Logger::Formatter
+  Format = "%s, [%s#%d/%s] %5s -- %s: %s\n"
+
+  def call(severity, time, progname, msg)
+    Format % [severity[0..0], format_datetime(time), $$, Thread.current,
+              severity, progname, msg2str(msg)]
+  end
+end
+
+MQRPC::logger.formatter = FormatterWithThread.new
