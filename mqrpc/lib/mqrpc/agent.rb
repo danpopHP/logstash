60644c27745914461bd9f19da75d18e1e384b334
jls@semicomplete.com <jls@semicomplete.com@c74e39cc-815a-11de-a6cd-7b297ead3933>
2009-11-15 16:40:19 +0000

    - Use an Operation instead of a sleepyloop when waiting for unsubscribe

--- mqrpc/lib/mqrpc/agent.rb
+++ mqrpc/lib/mqrpc/agent.rb
@@ -159,12 +159,17 @@ module MQRPC
       exchange = @mq.topic(@config.mqexchange, :durable => true)
       mq_q = @mq.queue(name, :durable => true)
       mq_q.bind(exchange, :key => "*")
-      mq_q.unsubscribe { @queues.delete(name) }
 
-      # wait for unsubscribe to finish; it's async
-      while @queues.member?(name)
-        sleep(0.1)
-      end
+      op = Operation.new
+      mq_q.unsubscribe { op.finished }
+      op.wait_until_finished
+      @queues.delete(name)
+
+      #mq_q.unsubscribe { @queues.delete(name) }
+      ## wait for unsubscribe to finish; it's async
+      #while @queues.member?(name)
+        #sleep(0.1)
+      #end
     end # def unsubscribe
 
     def subscribe_topic(name)
